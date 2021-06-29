0e3ab73df76a73caaddf24f7e091756b337e7b28
motoshira <51407064+motoshira@users.noreply.github.com>
2021-06-29 23:35:36 +0900

    Add skelton

---
+++ src/macros.lisp
@@ -0,0 +1,4 @@
+(defpackage #:cobblestone/macros
+  (:use #:cl))
+
+(in-package #:cobblestone/macros)
--- src/main.lisp
+++ src/main.lisp
@@ -4,3 +4,27 @@
   (:use #:cl))
 
 (in-package #:cobblestone/main)
+
+(defmacro ok (expr) nil)
+
+(defmacro ng (expr) nil)
+
+(defmacro pass (expr) nil)
+
+(defmacro fail (expr) nil)
+
+(defmacro is (expr expected) nil)
+
+(defmacro isnt (expr expected) nil)
+
+(defmacro is-expand (expr expected-sexp) nil)
+
+(defmacro is-conditon (expr expected-condition) nil)
+
+(defun run-test (test)
+  (declare (test-case test))
+  (the test-result)
+  nil)
+
+(defun run-tests (&rest test-casess)
+  nil)
