c17e1cb5241a8cd5005f02bd1a5206a5e2abd95d
motoshira <51407064+motoshira@users.noreply.github.com>
2021-07-01 23:22:06 +0900

    Add run-test

--- src/test.lisp
+++ src/test.lisp
@@ -14,6 +14,8 @@
   (fn nil :type function)
   (doc nil :type (or null string)))
 
+;; tODO: fixture
+
 (defmacro make-test ((&key name doc) &body body)
   `(%make-test
     ,@(when name
@@ -23,12 +25,22 @@
     :fn (lambda ()
           ,@body)))
 
-(defstruct (result (:constructor %%make-result))
+(defstruct (result (:constructor %make-result))
   (doc "" :type (or null string))
   (fn nil :type function)
-  (result nil :type (member :pass :fail :skip)))
+  (result nil :type (member :pass :fail :skip))
+  (error nil :type (or null condition)))
 
 (defun run-test (test-case)
-  (%%make-result :doc (test-doc test)
-                 :fn (test-fn test)
-                 :result result-key))
+  (with-slots (name fn doc) test-case
+    (multiple-value-bind (res err)
+        (block test-suite
+          (handler-bind ((error (lambda (c)
+                                  ;; とりあえず捕捉してfailにする
+                                  (return-from test-suite (values :fail c)))))
+            (funcall fn)
+            (values :pass nil)))
+      (%make-result :doc doc
+                    :fn fn
+                    :result res
+                    :error err))))
