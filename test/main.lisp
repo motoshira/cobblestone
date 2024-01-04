921a7175e32e37e64e9f4c395f2eb4076781a8fe
motoshira <51407064+motoshira@users.noreply.github.com>
2024-01-04 21:58:53 +0900

    Use function-based implementation

--- test/main.lisp
+++ test/main.lisp
@@ -4,32 +4,31 @@
 
 (in-package #:cobblestone/test/main)
 
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (defparameter *test-validator-spec*
-    '(("id"
-       (:required t
-        :message "id is required")
-       (:validate #'integerp
-        :message "id must be a integer"))
-      ("name"
-       (:required t
-        :message "name is required")
-       (:validate #'stringp
-        :message "name must be a string")))))
+(defparameter *test-validator-spec*
+  `(("id"
+     (:validate ,#'integerp
+      :message "id must be a integer"))
+    ("name"
+     (:validate ,#'stringp
+      :message "name must be a string"))))
 
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (defparameter *test-validator* (compile-validator #.*test-validator-spec*)))
+(defparameter *test-validator* (compile-validator *test-validator-spec*
+                                                  :key-test #'equal
+                                                  :required-keys '("id" "name")
+                                                  :required-message (lambda (key)
+                                                                      (format nil "~a is required" key))))
 
 (deftest validator-test
   (testing "missing keys"
     (let ((params '()))
       (ok (equalp (multiple-value-list
                    (validate *test-validator* params))
-                  `(nil (("id" . "id is required")
-                         ("name" . "name is required")))))))
+                  `((("id" . "id is required")
+                     ("name" . "name is required"))
+                    nil)))))
   (testing "pass"
     (let ((params '(("id" . 1)
                     ("name" . "motoshira"))))
       (ok (equalp (multiple-value-list
                    (validate *test-validator* params))
-                  `(,params nil))))))
+                  `(nil ,params))))))
