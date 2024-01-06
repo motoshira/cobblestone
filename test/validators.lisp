70fa685a39811c4a4200a67df65629a02d07169e
motoshira <51407064+motoshira@users.noreply.github.com>
2024-01-06 14:58:36 +0900

    Refine uuid validator test

--- test/validators.lisp
+++ test/validators.lisp
@@ -23,11 +23,18 @@
   (let* ((schema `(("id" . (,(uuid)))))
          (validator (compile-validator schema)))
     (testing "fail"
-      (let ((params '(("id" . "invalid-format-string"))))
-        (ok (equalp (multiple-value-list
-                     (validate validator params))
-                    '((("id" . "Invalid uuid format"))
-                      nil)))))
+      (testing "some string"
+        (let ((params '(("id" . "invalid-format-string"))))
+          (ok (equalp (multiple-value-list
+                       (validate validator params))
+                      '((("id" . "Invalid uuid format"))
+                        nil)))))
+      (testing "some garbage after uuid"
+        (let ((params '(("id" . "8B0F6081-CC5D-4C67-A815-A16A3C2C8153garbage"))))
+          (ok (equalp (multiple-value-list
+                       (validate validator params))
+                      '((("id" . "Invalid uuid format"))
+                        nil))))))
     (testing "pass"
       (let ((params '(("id" . "8B0F6081-CC5D-4C67-A815-A16A3C2C8153"))))
         (ok (equalp (multiple-value-list
